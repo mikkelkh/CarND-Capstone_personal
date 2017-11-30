bb0a35fc06c383d47ed0e3feaaea74498a6a8391
PeteHeine <repetepc@gmail.com>
2017-11-30 20:31:21 +0100

    Adding a few comments. Bug-fix to how images are stored.

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -43,7 +43,8 @@ class TLDetector(object):
 
         config_string = rospy.get_param("/traffic_light_config")
         
-        self.dirData = os.path.join("/home/pistol/DataFolder/SelfDriving/TraficLights")
+        # Folder of where images are stored. 
+        self.dirData = os.path.join("/home/pistol/DataFolder/SelfDriving/TrafficLights")
         self.config = yaml.load(config_string)
 
         self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', Int32, queue_size=1)
@@ -62,7 +63,8 @@ class TLDetector(object):
         self.waypoints_prepared = False
         self.waypoints_received = False
         self.idx_traffic_light_found = False
-
+        self.update_waypoint = False
+        
         rospy.spin()
 
     def pose_cb(self, msg):
@@ -72,6 +74,7 @@ class TLDetector(object):
         print("WAYPOINTS_RECEIVED")
         self.waypoints = waypoints
         self.waypoints_received = True
+        self.update_waypoint = True
 
     def traffic_cb(self, msg):
         self.lights = msg.lights
@@ -117,10 +120,11 @@ class TLDetector(object):
             int: index of the closest waypoint in self.waypoints
 
         """
-        #
+
+        # The closest waypoint is determiend using a KDtree - just to be difficult.
         useKDTree = True
-        
-        if(self.waypoints_prepared == False):
+        if(self.waypoints_prepared == False) or self.update_waypoint:
+            # Placing waypoints in list
             self.waypointlist = np.array([[waypoint.pose.pose.position.x,
                                            waypoint.pose.pose.position.y,
                                            waypoint.pose.pose.orientation.x,
@@ -128,15 +132,20 @@ class TLDetector(object):
                                            waypoint.pose.pose.orientation.z,
                                            waypoint.pose.pose.orientation.w] for waypoint in self.waypoints.waypoints])
             
+            # Determining the KDTree to faster measure distance to closest point
             if useKDTree:
                 self.tree = spatial.KDTree(self.waypointlist[:,0:2],1)
             self.waypoints_prepared = True
+            self.update_waypoint = False
             
+        
+        # Get closest idx. 
         if(self.waypoints_prepared):
             if useKDTree:
                 dist,idxs = self.tree.query(pose)
                 self.idx = idxs
             else:
+                # Otherwise calculate it
                 dist = np.sum(np.power(pose-self.waypointlist[:,0:2],2),axis=1)
                 self.idx = np.argmin(dist)
         else:
@@ -176,43 +185,49 @@ class TLDetector(object):
 
         idx_waypoint_traffic_light = -1
         
+        
+        # This is only processed if waypoints have been received. 
         if self.waypoints_received:
+            
             # List of positions that correspond to the line to stop in front of for a given intersection
             stop_line_positions = np.array(self.config['stop_line_positions'])
             #TODO find the closest visible traffic light (if one exists)
             idxs_waypoint_stop = self.get_closest_waypoint(stop_line_positions)
             
             
-            if(self.pose):
+            
+            # For a valid vehicle pose. Get the closest waypoint of vehicle.         
+            if(self.lights) and (self.pose):
                 vehicle_pose = np.array([[ self.pose.pose.position.x,self.pose.pose.position.y]])
                 idx_waypoint_car = self.get_closest_waypoint(vehicle_pose)        
                 
             
-            idx_traffic_light = np.argmax((idxs_waypoint_stop-idx_waypoint_car)>0)
-            idx_waypoint_traffic_light = idxs_waypoint_stop[idx_traffic_light]
-#            print(idxs_waypoint_stop,idx_waypoint_car,idxs_waypoint_stop-idx_waypoint_car,idx_traffic_light,idx_waypoint_traffic_light)
-        
-            if(self.lights):
+                idx_traffic_light = np.argmax((idxs_waypoint_stop-idx_waypoint_car)>0)
+                idx_waypoint_traffic_light = idxs_waypoint_stop[idx_traffic_light]
+                
+                # Get closest traffic light. 
                 tmpLight = self.lights[idx_traffic_light]
+                
+                # Get vehicle pose (position and orientation)
                 poseVehicle = self.pose.pose
                 
+                # Get pose of the waypoint closest to the traffic light. 
                 poseLight = self.waypointlist[idx_waypoint_traffic_light,:]
                 orientationLight = poseLight[2:]
                 positionLight = poseLight[:2]
                 
+                # Difference between vehicle position and traffic light position. 
                 diff_position = positionLight-np.array([ poseVehicle.position.x,poseVehicle.position.y])
-                # Use Pose
-                usePose = True
-                if usePose:
-                    o1 = orientationLight
-                    o2 = poseVehicle.orientation
-                    # Angle difference between light-orientation and vehicle-orientation
-                    q3 = np.quaternion(o1[0],o1[1],o1[2],o1[3]) * np.quaternion(o2.x,o2.y,o2.z,o2.w).inverse()
-                    diff_angle = euler_from_quaternion(quaternion.as_float_array(q3))
-                    diff_angle = diff_angle[2]
-                else:
-                    # NOT WORKING
-                    diff_angle = 1.5707963267948966-np.arctan2(diff_position[1],diff_position[0]) #????
+                
+                
+                # Difference in orientation (calculated in quaternions)
+                o1 = orientationLight
+                o2 = poseVehicle.orientation
+                # Angle difference between light-orientation and vehicle-orientation
+                q3 = np.quaternion(o1[0],o1[1],o1[2],o1[3]) * np.quaternion(o2.x,o2.y,o2.z,o2.w).inverse()
+                diff_angle = euler_from_quaternion(quaternion.as_float_array(q3))
+                diff_angle = diff_angle[2]
+                
                 # Distance between ligt and vehicle
                 diff_dist = np.sqrt(np.sum(np.power(diff_position,2)))
                 
@@ -223,9 +238,12 @@ class TLDetector(object):
                 in_view_angle = 0.13
                 
                 
-                # Store images for training.
+                # Store images with count, label, distance. 
+                # e.g. 0010_0_100.jpg is the name of image 10 with label 0 (red light) 100 meter from traffic light
                 if self.has_image:
                     
+                    # Save factor defines how often an image is stored.
+                    # Five-times more images are stored for yellow and green light 
                     save_factor = 10
                     saveImage = False
                     if (tmpLight.state==0): # RED LIGHT --> Slow save factor
@@ -235,23 +253,24 @@ class TLDetector(object):
                     elif(tmpLight.state==2):  # Green LIGHT --> Faster save factor
                         save_factor = 2
                     
-                    # Traffic light in view
+                    # Defining when a traffic light is in view
                     if (diff_dist < in_view_dist ) and (np.abs(diff_angle)<in_view_angle):          
                         saveImage = True
                         strState = str(tmpLight.state)
                         print("TrafficLight in view: ")
                     
-                    # Traffic light out of view
+                    # Defining when a traffic light is out of view
                     if (diff_dist > (in_view_dist+100) ) or (np.abs(diff_angle)>(in_view_angle+0.05)):
                         saveImage = True
                         save_factor = 10
                         strState = str(4)
                         print("TrafficLight definitly out off view: ")
                         
+                    # Stores images to folder defined in (self.dirData)
                     if saveImage and (np.mod(self.image_count,save_factor)==0):
                         #tmpName = str(np.round(time.time(),decimals=2)).replace(".","_")
                         tmpName = str(self.image_count).zfill(4)
-                        dirOut = os.path.join(self.dirData,tmpName+"_"+strState+"_"+ str(np.round(diff_dist)) + '.jpg')
+                        dirOut = os.path.join(self.dirData,tmpName+"_"+strState+"_"+ str(int(diff_dist)) + '.jpg')
                         cv2.imwrite(dirOut,self.camera_image)
                         print("SaveImage")
                     
